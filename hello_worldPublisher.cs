2f4a9601ac929c6a2448a402190e18e9c5c7401f
Jeffrey <dijkstrajeffrey@live.nl>
2021-12-07 15:28:44 +0100

    hallo

--- hello_worldPublisher.cs
+++ hello_worldPublisher.cs
@@ -51,7 +51,7 @@ public static class HelloWorldPublisher
         for (int count = 0; count < sampleCount; count++)
         {
             // Modify the data to be sent here
-            sample.msg = $"Moi! {count}";
+            sample.msg = $"HALLO! {count}";
             Console.WriteLine($"Writing HelloWorld, count {count}");
 
             writer.Write(sample);
